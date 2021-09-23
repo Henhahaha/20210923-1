c5d83305530c4f228f4cf84409ae7235e430c8d1
h4568 <h4568@HENRY-NB>
2021-09-24 01:02:27 +0800

    加入專案檔案。

---
+++ 20210923-1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _20210923_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double height, weight;
+
+            height = Convert.ToInt32(Console.ReadLine());
+            weight = Convert.ToInt32(Console.ReadLine());
+
+            double h = height / 100;
+            double ans = Math.Round(weight / (h*h),3);  //Math.Round(數值, 小數位數, MidpointRounding.AwayFromZero)
+         
+            Console.Write($"Height: {height} Weight: {weight} ,BMI is : {ans}");    
+
+            Console.Read();
+        }
+    }
+}
